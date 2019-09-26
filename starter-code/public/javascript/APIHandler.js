f1eebe159c3b13e73c3b42d2bd030038cb67b339
Patrick Weiss <patrick.weiss@officeone.team>
2019-09-26 16:37:40 +0200

    lab done

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,68 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.characterContainer = document.getElementsByClassName("characters-container")[0]
   }
 
-  getFullList () {
 
+  createCharacterHTML(character){
+    return   `
+    <div class="character-info">
+      <div class="name">${character.name}</div>
+      <div class="occupation">${character.occupation}</div>
+      <div class="cartoon">${character.weapon}</div>
+      <div class="weapon">${character.cartoon}</div>
+    </div>
+    `
   }
 
-  getOneRegister () {
-
+  getFullList() {
+    axios.get(this.BASE_URL+"/characters").then(
+      characters => {
+        console.log(characters.data);
+        let characterHTMLString =""
+        characters.data.forEach( character => {
+          characterHTMLString+= this.createCharacterHTML(character);
+        })
+        this.characterContainer.innerHTML=characterHTMLString
+      }
+    )
   }
 
-  createOneRegister () {
-
+  getOneRegister(id) {
+    axios.get(this.BASE_URL+"/characters/"+id).then(
+      character => {
+        console.log(character.data);
+        this.characterContainer.innerHTML=this.createCharacterHTML(character.data)
+        document.getElementById('fetch-one').style.backgroundColor = 'green';
+      }
+    ).catch(error => {document.getElementById('fetch-one').style.backgroundColor = 'red';})
   }
 
-  updateOneRegister () {
-
+  createOneRegister(characterData) {
+    axios.post(this.BASE_URL+"/characters",characterData).then(
+      character => {
+        console.log(character.data);
+        this.characterContainer.innerHTML=this.createCharacterHTML(character.data)
+      }
+    )
   }
 
-  deleteOneRegister () {
+  updateOneRegister (characterData,id) {
+    axios.patch(this.BASE_URL+"/characters/"+id,characterData).then(
+      character => {
+        console.log(character.data);
+        this.characterContainer.innerHTML=this.createCharacterHTML(character.data)
+      }
+    )
+  }
 
+  deleteOneRegister (id) {
+    axios.delete(this.BASE_URL+"/characters/"+id).then(
+      character => {
+        console.log(character.data);
+        this.characterContainer.innerHTML=this.createCharacterHTML(character.data)
+      }
+    ).catch(error => {document.getElementById('delete-one').style.backgroundColor = 'red';})
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,42 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
 
+  document.getElementById('fetch-all').addEventListener('click', function (event) {
+    charactersAPI.getFullList();
   });
 
   document.getElementById('fetch-one').addEventListener('click', function (event) {
-
+    const id = document.getElementsByName("character-id")[0].value;
+    console.log("want this:"+id);
+    charactersAPI.getOneRegister(id);
   });
 
   document.getElementById('delete-one').addEventListener('click', function (event) {
-
+    charactersAPI.deleteOneRegister(document.getElementsByName("character-id-delete")[0].value)
   });
 
   document.getElementById('edit-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault();
+    const character = {
+      name:document.getElementById("name-edit").value,
+      occupation:document.getElementById("occupation-edit").value,
+      weapon:document.getElementById("weapon-edit").value,
+      cartoon:document.getElementById("cartoon-edit").checked
+    }
+    console.log(character);
+    charactersAPI.updateOneRegister(character,document.getElementById("id-edit").value);
   });
 
   document.getElementById('new-character-form').addEventListener('submit', function (event) {
-
+    event.preventDefault();
+    const character = {
+      name:document.getElementById("name-create").value,
+      occupation:document.getElementById("occupation-create").value,
+      weapon:document.getElementById("weapon-create").value,
+      cartoon:document.getElementById("cartoon-create").checked
+    }
+    console.log(character);
+    charactersAPI.createOneRegister(character);
   });
 });
